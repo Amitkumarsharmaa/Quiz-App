2c2c93004c177733006c50e49710c204b5c4d80f
Amit Kumar Sharma <137784725+Amitkumarsharmaa@users.noreply.github.com>
2025-09-16 16:33:03 +0530

    first commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,47 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+   {
+        question:  " What are Frames?",
+        options:["They divide a webpage","They are the type of borders used for a webpage","They are the type of borders used by web browsers","They divide the browser into two or more pieces or panes"],
+        answer:4},
+{
+    question: "What is Vue JS?",
+    options:["Scripting Language","A framework","Programming Language","Directives"],
+    answer:2
+},
+{
+    question:"What is JSX in React?",
+    options:["JavaScript Execution"," JavaScript XML"," JavaScript Extra"," None of the mentioned"],answer:2
+},
+{
+    question:"What is React.js?",
+    options:["Open-source JavaScript back-end library","  JavaScript front-end library to create a database"," Free and open-source JavaScript front-end library"," None of the mentioned"],answer:3
+},
+{
+    question:"What is the main purpose of JSX in React?",
+    options:["Database Query Language","   JavaScript Function"," CSS Styling"," HTML-like syntax"],answer:4
+},
+{
+    question:"What is the purpose of ‘React.StrictMode’?",
+    options:["To add animations","    To improve performance","  To enhance UI"," To add additional validation checks"],answer:4
+}
+];
